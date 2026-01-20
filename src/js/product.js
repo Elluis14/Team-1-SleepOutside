b432518b9193028c2a58a422d46ffdeb1e1d8a87
vinickchristian <vinickmiamo19@gmail.com>
2026-01-20 20:58:50 +0100

    Fix logical bugs in product listing and details

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,11 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
+loadHeaderFooter();
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const category = getParam("category");
+const productId = getParam("product");
+const dataSource = new ProductData(category);
+const productDetails = new ProductDetails(productId, dataSource);
+productDetails.init();
