86029927e31734808228ccb888dfbf79aaa811f8
M-Ola <ola466@gmail.com>
2026-01-24 12:04:32 -0600

    Complete individual activity

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,6 +1,6 @@
-import { renderListWithTemplate } from "./utils.mjs";
+import { renderListWithTemplate } from './utils.mjs';
 
-function productCardTemplate(product) {
+/* function productCardTemplate(product) {
   return `
     <li class="product-card">
       <a href="../product_pages/index.html?category=${this.category}&product=${product.Id}">
@@ -21,8 +21,11 @@ export default class ProductList {
   }
 
   async init() {
-    const list = await this.dataSource.getData();
-    this.renderList(list);
+   // const list = await this.dataSource.getData();
+    // this.renderList(list);
+    const list = await this.dataSource.getData(this.category);
+
+  
   }
 
   renderList(list) {
@@ -34,4 +37,41 @@ export default class ProductList {
 
   }
 
+} */
+
+
+
+function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+      <a href="/product_pages/?product=${product.Id}">
+        <img src="${product.Images.PrimaryMedium}" alt="${product.Name}">
+        <h3>${product.Brand.Name}</h3>
+        <p>${product.NameWithoutBrand}</p>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+    `;
+}
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData(this.category);
+    this.renderList(list);
+    document.querySelector('.title').textContent = this.category;
+  }
+
+  renderList(list) {
+    // const htmlStrings = list.map(productCardTemplate);
+    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+
+    // apply use new utility function instead of the commented code above
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
 }
