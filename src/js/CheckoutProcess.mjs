9f43597623659bf8f4cb54e5031f9c0489179627
M-Ola <ola466@gmail.com>
2026-02-01 20:20:46 -0600

    Finished individual task 4

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,4 +1,6 @@
 import { getLocalStorage } from './utils.mjs';
+import { setLocalStorage } from './utils.mjs';
+import { alertMessage, removeAllAlerts } from './utils.mjs';
 import ExternalServices from './ExternalServices.mjs';
 
 const services = new ExternalServices();
@@ -82,21 +84,30 @@ export default class CheckoutProcess {
     orderTotal.innerText = `$${this.orderTotal.toFixed(2)}`;
   }
 
-  async checkout() {
-    const formElement = document.forms['checkout'];
-    const order = formDataToJSON(formElement);
+  
+async checkout() {
+    const formElement = document.forms["checkout"];
 
-    order.orderDate = new Date().toISOString();
-    order.orderTotal = this.orderTotal;
-    order.tax = this.tax;
-    order.shipping = this.shipping;
-    order.items = packageItems(this.list);
-    //console.log(order);
-
+    const json = formDataToJSON(formElement);
+    // add totals, and item details
+    json.orderDate = new Date();
+    json.orderTotal = this.orderTotal;
+    json.tax = this.tax;
+    json.shipping = this.shipping;
+    json.items = packageItems(this.list);
+    console.log(json);
     try {
-      const response = await services.checkout(order);
-      console.log(response);
+      const res = await services.checkout(json);
+      console.log(res);
+      setLocalStorage("so-cart", []);
+      location.assign("/checkout/success.html");
     } catch (err) {
+      // remove any preexisting alerts.
+      removeAllAlerts();
+      for (let message in err.message) {
+        alertMessage(err.message[message]);
+      }
+
       console.log(err);
     }
   }
